20db55c2a660cd7940b93ba98a0e3b47772d534f
Nick Fuenmayor <in.nickf@gmail.com>
2023-04-23 21:50:22 +0200

    (feat): mirrorful installed & font variables added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,70 @@
+const { Tokens } = require("./.mirrorful/theme_cjs.js");
+
+const fontSizes = Object.entries(Tokens.fontSizes).reduce(
+  (acc, [key, value]) => {
+    const lineHeight = Tokens.lineHeights[key];
+    acc[key] = lineHeight ? [value, { lineHeight }] : value;
+    return acc;
+  },
+  {}
+);
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/renderer/index.html", "./src/renderer/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      ...Tokens.colors,
+    },
+    fontFamily: {
+      sans: [
+        "paragraph",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+      ],
+      serif: [
+        "heading",
+        "ui-serif",
+        "Georgia",
+        "Cambria",
+        '"Times New Roman"',
+        "Times",
+        "serif",
+      ],
+      mono: [
+        "ui-monospace",
+        "SFMono-Regular",
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        '"Liberation Mono"',
+        '"Courier New"',
+        "monospace",
+      ],
+    },
+    fontSize: fontSizes,
+    fontWeight: {
+      normal: "normal",
+      bold: "bold",
+    },
+    lineHeight: Tokens.lineHeights,
     extend: {},
   },
   plugins: [],
